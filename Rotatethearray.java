ffd84ba51453b10bb3c954a043597ad14f849a72
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-08-09 15:44:30 +0530

    Create Rotatethearray.java

---
+++ Rotatethearray.java
@@ -0,0 +1,15 @@
+/*SAMPLE INPUT 1:
+      Input: nums = [1,2,3,4,5,6,7], k = 3
+      Output: [5,6,7,1,2,3,4]
+     */
+class Solution {
+    public void rotate(int[] nums, int k) {
+       for(int incre=-1;incre<k;incre++)
+       {
+           int temp=nums[0];
+           for(int i=0;i<nums.length-1;i++)
+               nums[i]=nums[i+1];
+           nums[nums.length-1]=temp;
+
+    }
+}
