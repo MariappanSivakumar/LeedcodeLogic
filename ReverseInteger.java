984cdd01c4194b345db1cab39d3a233573142588
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-08-13 08:03:09 +0530

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,37 +1,19 @@
-/*
-Example 1:
-
-Input: x = 123
-Output: 321
-Example 2:
-
-Input: x = -123
-Output: -321
-Example 3:
-
-Input: x = 120
-Output: 21
-*/
-
-
 class Solution {
     public int reverse(int x) {
-        int sum=0;
-        int value=x;
+        long sum=0;
+        int minus=0;
         if(x<0)
-        for(int i=0;i<=(""+x).length();i++)
-        {
-            sum=(sum*10)+x%10;
-            x/=10;
-        }
+            minus=1;
+        x=Math.abs(x);
         while(x>0)
         {
-            sum=(sum*10)+x%10;
-            x/=10;
-            if(sum==value)
-                break;
+                sum=(sum*10)+x%10;
+                x/=10;
         }
-            
-        return sum;
+        if(sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE)
+            sum=0;
+            if(minus==1)
+                sum*=-1;
+        return (int)sum;
     }
 }
