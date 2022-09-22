e2e1af4e7232fc848d7dcfec19e26b0f200654f9
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-09-22 09:58:37 +0100

    Create Counting Words With a Given Prefix.java

---
+++ Counting Words With a Given Prefix.java
@@ -0,0 +1,21 @@
+/*
+Example 1:
+
+Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+Example 2:
+
+Input: words = ["leetcode","win","loops","success"], pref = "code"
+Output: 0
+Explanation: There are no strings that contain "code" as a prefix.
+*/
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count=0;
+        for(String word:words)
+            if(word.indexOf(pref)==0)
+                count++;
+        return count;
+    }
+}
