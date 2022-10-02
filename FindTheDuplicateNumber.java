64d0758d8faa5405836dd49601c034011e69deab
Mariappan <97144165+MariappanSivakumar@users.noreply.github.com>
2022-10-02 12:05:09 +0530

    Create FindTheDuplicateNumber.java

---
+++ FindTheDuplicateNumber.java
@@ -0,0 +1,21 @@
+/*
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+*/
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        java.util.Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
+            if(nums[i]==nums[i+1])
+                return nums[i];
+        return 0;
+    }
+}
