f899012bbccd6aa28368ead90d48ec19a28411f3
Mariappan <97144165+MariappanSivakumar@users.noreply.github.com>
2022-10-14 07:10:05 +0530

    Create CalculateMoneyInLeedcode.java

---
+++ CalculateMoneyInLeedcode.java
@@ -0,0 +1,34 @@
+/*
+Example 1:
+
+Input: n = 4
+Output: 10
+Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+Example 2:
+
+Input: n = 10
+Output: 37
+Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+Example 3:
+
+Input: n = 20
+Output: 96
+Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+*/
+class Solution {
+    public int totalMoney(int n) {
+        int count=0;
+        int sum=0; int start=1;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=count+start;
+            start++;
+            if(i%7==0)
+            {
+                count++;
+                start=1;
+            }
+        }
+        return sum;
+    }
+}
